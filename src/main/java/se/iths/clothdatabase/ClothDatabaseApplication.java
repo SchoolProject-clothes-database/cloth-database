c6e2a93d130bc82a857a8d92cf64ca0c47c49d4c
Lukas <lukas.ikvm1@gmail.com>
2022-03-10 15:55:58 +0100

    Initial commit

---
+++ src/main/java/se/iths/clothdatabase/ClothDatabaseApplication.java
@@ -0,0 +1,13 @@
+package se.iths.clothdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClothDatabaseApplication.class, args);
+    }
+
+}
