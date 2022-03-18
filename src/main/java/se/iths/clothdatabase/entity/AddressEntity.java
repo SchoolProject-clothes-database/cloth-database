ad55926d665216b36e0338bf8f83e13d06c035eb
ReymartNarvaez <Reymart_narvaez@hotmail.com>
2022-03-18 13:17:49 +0100

    Added variables in Entity classes

--- src/main/java/se/iths/clothdatabase/entity/AddressEntity.java
+++ src/main/java/se/iths/clothdatabase/entity/AddressEntity.java
@@ -1,4 +1,99 @@
 package se.iths.clothdatabase.entity;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class AddressEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String street;
+    private int zipCode;
+    private int houseNumber;
+    private String country;
+    private String province;
+    private String city;
+    private String phoneNumber;
+    @OneToMany(mappedBy = "address", cascade = CascadeType.ALL)
+    private List<UserDetailsEntity> userDetails = new ArrayList<>();
+
+    public void addUserDetails(UserDetailsEntity userDetail) {
+        userDetails.add(userDetail);
+        userDetail.setAddress(this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(int houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public List<UserDetailsEntity> getUserDetails() {
+        return userDetails;
+    }
+
+    public void setUserDetails(List<UserDetailsEntity> userDetails) {
+        this.userDetails = userDetails;
+    }
 }
--- src/main/java/se/iths/clothdatabase/entity/UserEntity.java
+++ src/main/java/se/iths/clothdatabase/entity/UserEntity.java
@@ -1,4 +1,77 @@
 package se.iths.clothdatabase.entity;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String userName;
+    private String role;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<CartEntity> carts = new ArrayList<>();
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "userDetail_id", nullable = false)
+    private UserDetailsEntity userDetail;
+    @OneToOne(fetch = FetchType.LAZY,
+            cascade =  CascadeType.ALL,
+            mappedBy = "user")
+    private PaymentEntity payment;
+
+    public void addCart(CartEntity cart) {
+        carts.add(cart);
+        cart.setUser(this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public List<CartEntity> getCarts() {
+        return carts;
+    }
+
+    public void setCarts(List<CartEntity> carts) {
+        this.carts = carts;
+    }
+
+    public UserDetailsEntity getUserDetail() {
+        return userDetail;
+    }
+
+    public void setUserDetail(UserDetailsEntity userDetail) {
+        this.userDetail = userDetail;
+    }
+
+    public PaymentEntity getPayment() {
+        return payment;
+    }
+
+    public void setPayment(PaymentEntity payment) {
+        this.payment = payment;
+    }
 }
