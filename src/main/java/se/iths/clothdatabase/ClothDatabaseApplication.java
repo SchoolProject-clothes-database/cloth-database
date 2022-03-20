75aa250b35d7abca6667a676206632c1d396b7df
Sempron <lukas.ikvm1@gmail.com>
2022-03-21 00:19:12 +0100

    add sample data for testing purposes

--- src/main/java/se/iths/clothdatabase/ClothDatabaseApplication.java
+++ src/main/java/se/iths/clothdatabase/ClothDatabaseApplication.java
@@ -4,8 +4,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import se.iths.clothdatabase.entity.AddressEntity;
 import se.iths.clothdatabase.entity.RoleEntity;
+import se.iths.clothdatabase.entity.UserDetailsEntity;
+import se.iths.clothdatabase.entity.UserEntity;
+import se.iths.clothdatabase.repository.AddressRepository;
 import se.iths.clothdatabase.repository.RoleRepository;
+import se.iths.clothdatabase.repository.UserDetailsRepository;
+import se.iths.clothdatabase.repository.UserRepository;
 
 @SpringBootApplication
 public class ClothDatabaseApplication {
@@ -19,6 +25,21 @@ public class ClothDatabaseApplication {
         return (args) -> {
             roleRepository.save(new RoleEntity("ROLE_ADMIN"));
             roleRepository.save(new RoleEntity("ROLE_USER"));
+
+
+
+        };
+    }
+
+    @Bean
+    public CommandLineRunner setUpAddress(AddressRepository addressRepository, UserDetailsRepository userDetailsRepository, UserRepository userRepository, RoleRepository roleRepository){
+        return (args) -> {
+            AddressEntity address1 = new AddressEntity("city",2223,32,"country","province","street","phonenumber");
+            UserDetailsEntity userDetailsEntity1 = new UserDetailsEntity("firstname", "lastname", "email" );
+            UserEntity userEntity = new UserEntity("username","password");
+            address1.addAddress(userDetailsEntity1);
+            userEntity.addDetails(userDetailsEntity1);
+            addressRepository.save(address1);
         };
     }
 
