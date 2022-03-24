bb4a2c9c271e1fe1cf1f5608a4372e9afa8c7252
Sempron <lukas.ikvm1@gmail.com>
2022-03-24 13:48:59 +0100

    add more role security test

--- src/test/java/se/iths/clothdatabase/security/UserSecurityTests.java
+++ src/test/java/se/iths/clothdatabase/security/UserSecurityTests.java
@@ -1,12 +1,18 @@
 package se.iths.clothdatabase.security;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.*;
 import org.springframework.test.context.ContextConfiguration;
@@ -16,12 +22,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import se.iths.clothdatabase.controller.ProductController;
+import se.iths.clothdatabase.entity.ProductEntity;
+import se.iths.clothdatabase.service.ProductService;
 
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultHandlers.exportTestSecurityContext;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -34,6 +45,9 @@ public class UserSecurityTests {
 
     private MockMvc mvc;
 
+    @MockBean
+    ProductService productService;
+
     @BeforeEach
     public void setup(){
         mvc = MockMvcBuilders
@@ -44,17 +58,34 @@ public class UserSecurityTests {
     }
     @Test
     @WithMockUser()
-    public void noneUserIsNotAuthenticated() throws Exception{
+    void noneUserIsNotAuthenticated() throws Exception{
         mvc
                 .perform(formLogin().password("invalid"))
                 .andExpect(unauthenticated());
     }
+
     @Test
     @WithMockUser(username = "user",password = "pass",roles = "USER")
     void userShouldNotBeAbleToAccessAdminLevelPage() throws Exception{
         mvc
-                .perform(formLogin("/admin"))
+                .perform(get("/admin"))
                 .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(username = "user",password = "pass",roles = "USER")
+    void userShouldBeAbleToAccessUserLevelPage() throws Exception{
+        mvc
+                .perform(get("/category/findAll"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "user",password = "pass",roles = "ADMIN")
+    void adminUserShouldBeAbleToAccessUserLevelPage() throws Exception{
+        mvc
+                .perform(get("/category/findAll"))
+                .andExpect(status().isOk());
     }
 
 }
