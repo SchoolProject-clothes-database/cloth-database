7ab75aeb86dea2b771789387497195f60d879eba
Sempron <lukas.ikvm1@gmail.com>
2022-03-29 14:02:09 +0200

    add productsAddsCorrectlyToCategories test

---
+++ src/test/java/se/iths/clothdatabase/productEntityTests/ProductTests.java
@@ -0,0 +1,29 @@
+package se.iths.clothdatabase.productEntityTests;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import se.iths.clothdatabase.entity.CategoryEntity;
+import se.iths.clothdatabase.entity.ProductEntity;
+import se.iths.clothdatabase.repository.ProductRepository;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+public class ProductTests {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Test
+    void productsAddsCorrectlyToCategories(){
+        ProductEntity productEntity = new ProductEntity("socks",20,2);
+        CategoryEntity categoryEntity = new CategoryEntity("sock","cloth");
+        productEntity.addCategory(categoryEntity);
+
+        assertThat(productEntity.getCategory().getCategoryName()).isEqualTo("sock");
+        assertThat(categoryEntity.getProducts().get(0).getProductName()).isEqualTo("socks");
+    }
+
+}
