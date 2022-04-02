e9d0db44be6aed826a73138c7c7de95836d51905
Sempron <lukas.ikvm1@gmail.com>
2022-04-02 12:40:40 +0200

    add basic exception handling

---
+++ src/main/java/se/iths/clothdatabase/exception/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package se.iths.clothdatabase.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private ResponseEntity<Object> buildResponseEntity(ApiError apiError){
+        return new ResponseEntity<>(apiError, apiError.getStatus());
+    }
+
+
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<Object> handleAll(Exception ex, WebRequest request) {
+        logger.info(ex.getClass().getName());
+        logger.error("Error: ", ex);
+        String errorMessage = "An unexpected error occurred.";
+
+        return buildResponseEntity(new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, errorMessage, ex));
+    }
+
+
+}
